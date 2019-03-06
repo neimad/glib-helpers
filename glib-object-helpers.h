ae079a9747b5e988d0e684f12a4203b84350f8e3
Damien Flament <damien.flament@gmx.com>
2019-03-06 18:39:30 +0100

    Use SPDX license identfier for license notice

--- glib-object-helpers.h
+++ glib-object-helpers.h
@@ -1,21 +1,6 @@
-/*
- * Copyright © 2019 Damien Flament
- *
- * This file is part of GLib Helpers.
- *
- * GLib Helpers is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 2.1 of the License, or
- * (at your option) any later version.
- *
- * GLib Helpers is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with GLib Helpers.  If not, see <https://www.gnu.org/licenses/>.
- */
+// SPDX-License-Identifier: LGPL-2.1-or-later
+// Copyright © 2019 Damien Flament
+// This file is part of GLib Helpers.
 
 #ifndef __GLIB_GOBJECT_HELPERS_H__
 #define __GLIB_GOBJECT_HELPERS_H__
